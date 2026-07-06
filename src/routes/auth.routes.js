122a2446803b2f4d81dc113c84ff895164a3c760
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-07-06 18:04:27 +0530

    created login user controller with the help of jwt and also fixing its routes correctly

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,6 +8,14 @@ const authRouter = Router()
  * @access public
 */
 
-authRouter.post("register", authController.registerUserController)
+authRouter.post("/register", authController.registerUserController)
+
+/**
+ * @route POST/api/auth/login
+ * @description login user with email and password
+ * @access public
+ */
+
+authRouter.post("/login", authController.loginUserController)
 
 module.exports = authRouter
