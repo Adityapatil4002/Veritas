9470c3001aeb340bb2ee68638aa3257af3a579ef
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-07-06 16:20:21 +0530

    created register controller with the help of jwt

--- src/Controller/auth.controller.js
+++ src/Controller/auth.controller.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 
 /** 
@@ -24,6 +26,30 @@ async function registerUserController(req, res) {
             message: "Account is already exists with this email address or username"
         })
     }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    })
+
+    const token = user.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        {expiresIn: "1d"}
+    )
+
+    res.cookie("token", token)
+    res.status(201).json({
+        message: "user registeres successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
 }
 
 module.exports = {
