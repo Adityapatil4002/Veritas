1ef0b4f6cb5ff3bbf68b150ab7d58a7e230a73ca
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-07-11 17:46:03 +0530

    The basic login page is made and the basic style is also given to it

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,4 +1,15 @@
 import { createBrowserRouter } from "react-router"
+import Login from "./features/auth/Pages/login";
+import Register from "./features/auth/Pages/register"
 
 
-export const router  = createBrowserRouter
+export const router = createBrowserRouter([
+    {
+        path: "/Login",
+        element:<Login/>
+    },
+    {
+        path: "/Register",
+        element: <Register/>
+    }
+])
---
+++ Frontend/src/features/auth/Pages/login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "../auth.form.scss"
+const Login = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <main>
+            <div className="forom-container">
+                <h1>Login</h1>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id = "email" name = "email" placeholder='Enter you email address' />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id = "password" placeholder='enter the password' />
+                    </div>
+                    <button className='button primary-button'>Login</button>
+                </form>
+            </div>
+      </main>
+  )
+}
+
+export default Login
