a4015effba7d9c7fad8ac78359e79a7ccc5a4d02
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-07-12 01:21:49 +0530

    created functions for managing the user activities like login, logut, register with the help of hooks and also managing the loading part of the frontend

---
+++ Frontend/src/features/auth/services/hooks/useAuth.js
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { AuthContext } from "../../auth.context";
+import { login, register, logout, getMe } from "../services/auth.api";
+
+
+export const useAuth = () => {
+    const context = useContext(AuthContext)
+    const { user, setUser, loading, setLoading } = context
+    
+    const handlelogin = async ({ email, password }) => {
+        setLoading(true)
+        const data = await login({ email, password })
+
+        setUser(data.user)
+        setLoading(false)
+        
+    }
+    const handleregister = async ({ username, email, password }) => {
+        setLoading(true)
+        const data = await register({ username, email, password })
+        setUser(data.user)
+        setLoading(false)
+    }
+
+    const handlelogout = async () => {
+        setLoading(true)
+        const data = await logout()
+        setUser(null)
+        setLoading(false)
+    }
+
+    return {user, loading, handleregister, handlelogout, handlelogin}
+}
