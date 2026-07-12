6a52c11c592f91948167b3f406d67bb7c4b1b6c5
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-07-12 17:49:22 +0530

    routes are fixed if new user came and they refresh on home page then they have to relogin

--- Frontend/src/features/auth/Pages/register.jsx
+++ Frontend/src/features/auth/Pages/register.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useNavigate,Link } from 'react-router'
-
+import { useAuth } from '../services/hooks/useAuth'
 
 const Register = () => {
 
   const navigate = useNavigate()
-  
+  const [username, setusername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
+  const {loading, handleRegister} = useAuth()
     const handleSubmit = (e) => {
       e.preventDefault();
-    };
+      handleRegister({ username, email, password })
+      navigate("/")
+  };
+  
+  if (loading) {
+    return (<main><h1>Loading....</h1></main>)
+  }
 
     
   return (
@@ -21,6 +30,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="username">Username</label>
             <input
+              onChange={(e) => {
+                setusername(e.target.value);
+              }}
               type="text"
               id="username"
               name="username"
@@ -30,6 +42,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               type="email"
               id="email"
               name="email"
@@ -39,15 +54,20 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               type="password"
               id="password"
               placeholder="enter the password"
             />
           </div>
           <button className="button primary-button">Register</button>
-              </form>
-              
-        <p>Already have an account? <Link to={"/login"}>login</Link></p>
+        </form>
+
+        <p>
+          Already have an account? <Link to={"/login"}>login</Link>
+        </p>
       </div>
     </main>
   );
--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,10 +1,13 @@
 import { createContext, useState } from "react";
 
+
 export const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(false)
+
+    
 
     return (
         <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
@@ -12,3 +15,4 @@ export const AuthProvider = ({ children }) => {
         </AuthContext.Provider>
     )
 }
+
