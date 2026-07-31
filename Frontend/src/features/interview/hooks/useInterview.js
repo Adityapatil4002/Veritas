247680c4566b29a90d771f45435ceda1bf753de3
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-07-31 22:36:07 +0530

    managed the hooks to generate report and getting the report

---
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -0,0 +1,45 @@
+import { getAllInterviewReports, getInterviewReportById, generateInterviewReport} from "../services/interview.api"
+import { useContext } from "react"
+import {InterviewContext} from "../interview.context.jsx"
+
+
+export const useInterview = () => {
+    const context = useContext(InterviewContext)
+
+    if(!context) {
+        throw new Error("useInterview must be used within an InterviewProvider")
+    }
+
+    const { loading, setLoading, report, setReport, reports, setReports } = context
+
+    const generateReport = async ({ jobDescription, selfDescription, resumeFile }) => { 
+        setLoading(true)
+        try {
+            const response = await generateInterviewReport({ jobDescription, selfDescription, resumeFile })
+            setReport(response.interviewReport)
+        } catch (error) {
+            console.error("Error generating interview report:", error)
+        } finally {
+            setLoading(false)
+        }
+    
+    }
+
+    const getReportById = async (interviewId) => {
+        setLoading(true)
+        try {
+            const response = await getInterviewReportById(interviewId)
+            setReport(response.interviewReport)
+        } catch (error) {
+            console.error("Error getting interview report by Id")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const getReports = async () => {
+        
+    }
+
+    return {loading, report, reports, generateReport, getReportById, getReports}
+}
