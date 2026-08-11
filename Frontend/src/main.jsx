03c4bdbd00f20d2f365665e6a4b3080f2aac170d
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-08-11 15:36:26 +0530

    fixed

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./style.scss"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./style.scss";
+// 1. Import BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* 2. Wrap your App component inside the Router */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
