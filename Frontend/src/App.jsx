a9ff2eab08fa6934cae983fe8352d23d642ba167
Adityapatil4002 <patiladitya6062004@gmail.com>
2026-08-11 16:54:56 +0530

    fixed 4

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import { RouterProvider } from "react-router"
-import { router } from "./app.routes.jsx"
-import { AuthProvider } from "./features/auth/auth.context.jsx"
-import { InterviewProvider } from "./features/interview/interview.context.jsx"
-
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app.routes.jsx";
+import { AuthProvider } from "./features/auth/auth.context.jsx";
+import { InterviewProvider } from "./features/interview/interview.context.jsx";
 
 function App() {
-
   return (
     <AuthProvider>
       <InterviewProvider>
@@ -16,4 +13,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./style.scss";
-// 1. Import BrowserRouter
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* 2. Wrap your App component inside the Router */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
